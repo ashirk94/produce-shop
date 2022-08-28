8ea92659065f76e8ee2d61a394c818ea7fe2908a
Alan Shirk <ashirk94@gmail.com>
2022-08-27 18:00:31 -0700

    Fixed auth bug and provided error messages

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,65 +1,80 @@
-import React, {useRef, useContext, useState, useEffect} from 'react'
+import React, { useRef, useContext, useState, useEffect } from 'react'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
 import ThemeContext from '../../contexts/ThemeContext'
 import { useAuth } from '../../contexts/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Login() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const {theme} = useContext(ThemeContext)
-    const {currentUser, login} = useAuth()
-    const [error, setError] = useState('')
+	const emailRef = useRef()
+	const passwordRef = useRef()
+	const { theme } = useContext(ThemeContext)
+	const { currentUser, login } = useAuth()
+	const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+	const navigate = useNavigate()
 
-    useEffect(() => {
-        if(currentUser) {
-            navigate('/')
-        }
-    }, [currentUser, navigate])
+	useEffect(() => {
+		if (currentUser) {
+			navigate('/')
+		}
+	}, [currentUser, navigate])
 
-    async function handleSubmit(e) {
-        e.preventDefault()
+	async function handleSubmit(e) {
+		e.preventDefault()
+        setError('')
 
-        try {
-            setError('')
+		try {
             setLoading(true)
-            await login(emailRef.current.value, passwordRef.current.value)
-            navigate('/store')
-        } catch {
-            setError('Failed to sign in')
-        }
+			let loginStatus = await login(
+				emailRef.current.value,
+				passwordRef.current.value
+			)
+			if (loginStatus) {
+				navigate('/')
+			} else {
+                setError(loginStatus)
+            }
+        } catch(e) {
+            setError(e)
+		} 
         setLoading(false)
 
-    }
+	}
 
-  return (
-    <div>
-        <Card className="signup-card" id={theme}>
-            <Card.Body id={theme} className="signup-card">
-                <h2 className="text-center mb-4">Log In</h2>
-                {/* {currentUser && 'Current User: ' + currentUser.email} */}
-                {error && <Alert variant="danger">{error}</Alert>}
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group id="email">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} required />
-                    </Form.Group>
-                    <Form.Group id="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} required />
-                    </Form.Group>
+	return (
+		<div>
+			<Card className='signup-card' id={theme}>
+				<Card.Body id={theme} className='signup-card'>
+					<h2 className='text-center mb-4'>Log In</h2>
+					{/* {currentUser && 'Current User: ' + currentUser.email} */}
+					{error && <Alert variant='danger'>{error}</Alert>}
+					<Form onSubmit={handleSubmit}>
+						<Form.Group id='email'>
+							<Form.Label>Email</Form.Label>
+							<Form.Control
+								type='email'
+								ref={emailRef}
+								required
+							/>
+						</Form.Group>
+						<Form.Group id='password'>
+							<Form.Label>Password</Form.Label>
+							<Form.Control
+								type='password'
+								ref={passwordRef}
+								required
+							/>
+						</Form.Group>
                     <Button disabled={loading} className="w-100 mt-2" type="submit">Log In</Button>
-                </Form>
-                <div className="w-100 text-center mt-3">
-                    <Link to='/forgot-password'>Forgot Password?</Link>
-                    </div>
-            </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2">
-            Need an account? <Link to='/signup'>Sign Up</Link>
-        </div>
-    </div>
-  )
+					</Form>
+					<div className='w-100 text-center mt-3'>
+						<Link to='/forgot-password'>Forgot Password?</Link>
+					</div>
+				</Card.Body>
+			</Card>
+			<div className='w-100 text-center mt-2'>
+				Need an account? <Link to='/signup'>Sign Up</Link>
+			</div>
+		</div>
+	)
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,88 +1,105 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { auth } from '../firebase'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendPasswordResetEmail, updateEmail, updatePassword } from 'firebase/auth'
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	sendPasswordResetEmail,
+	updateEmail,
+	updatePassword
+} from 'firebase/auth'
 
 const AuthContext = React.createContext()
+
+let authError
+
+export function getAuthError() {
+	return authError
+}
 
 export function useAuth() {
 	return useContext(AuthContext)
 }
 
-
 function changeEmail(email) {
-    updateEmail(auth.currentUser, email)
+	updateEmail(auth.currentUser, email)
 }
 
 function changePassword(password) {
-    updatePassword(auth.currentUser, password)
+	updatePassword(auth.currentUser, password)
 }
 
-    function signup(email, password) {
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed in 
-          //const user = userCredential.user;
-          console.log('User created successfully')
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log(errorMessage, errorCode)
-        })
+async function signup(email, password) {
+	let errorMsg = 'Error'
+	try {
+		await createUserWithEmailAndPassword(auth, email, password)
+	} catch (error) {
+		const errorCode = error.code
+		const errorMessage = error.message
+		console.error(errorMessage, errorCode)
+		errorMsg = errorMessage
+	}
+	if (errorMsg !== 'success') {
+		throw errorMsg
+	}
 }
 
-function login(email, password) {
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      //const user = userCredential.user;
-      console.log('User logged in successfully')
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log(errorMessage, errorCode)
-    })
+async function login(email, password) {
+	let errorMsg = 'Error'
+	try {
+		await signInWithEmailAndPassword(auth, email, password)
+		return true
+	} catch (error) {
+		const errorCode = error.code
+		const errorMessage = error.message
+		console.error(errorMessage, errorCode)
+		errorMsg = errorMessage
+	}
+	if (errorMsg !== 'success') {
+		throw errorMsg
+	}
 }
 
 function logout() {
-    signOut(auth).then(() => {
-        console.log('User logged out successfully')
-      }).catch((error) => {
-        console.log(error)
-      })
+	signOut(auth)
+		.then(() => {
+			return true
+		})
+		.catch((error) => {
+			console.error(error)
+			return false
+		})
 }
 
 function resetPassword(email) {
-    sendPasswordResetEmail(auth, email)
+	sendPasswordResetEmail(auth, email)
 }
-
 
 export function AuthProvider({ children }) {
 	const [currentUser, setCurrentUser] = useState()
-    const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user)
-            setLoading(false)
-        })
-        return unsubscribe
-    }, [])
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user)
+			setLoading(false)
+		})
+		return unsubscribe
+	}, [])
 
 	const value = {
 		currentUser,
-        signup,
-        login,
-        logout,
-        resetPassword,
-        changeEmail,
-        changePassword
+		signup,
+		login,
+		logout,
+		resetPassword,
+		changeEmail,
+		changePassword
 	}
 
 	return (
-        <AuthContext.Provider value={value}>
-            {!loading && children}
-        </AuthContext.Provider>
-    )
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	)
 }
