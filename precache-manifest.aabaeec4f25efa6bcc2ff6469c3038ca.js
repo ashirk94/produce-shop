ae493a2b65c5f518033bbbfab37e400ca3fa180d
Alan Shirk <ashirk94@gmail.com>
2022-09-27 12:04:56 -0700

    Updates

---
+++ precache-manifest.aabaeec4f25efa6bcc2ff6469c3038ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "971547e5051d6c273b6e",
+    "url": "/produce-shop/static/css/main.92a29506.chunk.css"
+  },
+  {
+    "revision": "971547e5051d6c273b6e",
+    "url": "/produce-shop/static/js/main.971547e5.chunk.js"
+  },
+  {
+    "revision": "e4911fa870404ffda1b2",
+    "url": "/produce-shop/static/css/1.05a31eeb.chunk.css"
+  },
+  {
+    "revision": "e4911fa870404ffda1b2",
+    "url": "/produce-shop/static/js/1.e4911fa8.chunk.js"
+  },
+  {
+    "revision": "34ce230d3c3beaf592e9",
+    "url": "/produce-shop/static/js/runtime~main.34ce230d.js"
+  },
+  {
+    "revision": "1277d03b023bf8241c3ee1c68b4f0973",
+    "url": "/produce-shop/index.html"
+  }
+];
